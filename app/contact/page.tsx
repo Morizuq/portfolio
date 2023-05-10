77a1b313671c4fb938565e0e98c4e81422ecf866
Morizuq <morizuqshoneye@gmail.com>
2023-05-10 13:14:01 +0100

    add links

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -31,7 +31,9 @@ export default function Contact() {
 
   return (
     <div className="p-8 sm:p-10">
-      <h2 className="uppercase sm:text-4xl text-3xl text-center mb-9 grad">Contact Me</h2>
+      <h2 className="uppercase sm:text-4xl text-3xl text-center mb-9 grad">
+        Contact Me
+      </h2>
       <div className="flex sm:flex-row flex-col justify-between">
         {submitted ? (
           <div className="h-[5rem] my-auto api-card">
@@ -49,15 +51,29 @@ export default function Contact() {
             <label htmlFor="name" className="label">
               Your Name:
             </label>
-            <input type="text" name="name" id="name" className="input w-[94%] sm:w-[80%]" />
+            <input
+              type="text"
+              name="name"
+              id="name"
+              className="input w-[94%] sm:w-[80%]"
+            />
             <label htmlFor="email" className="label">
               Your Email:
             </label>
-            <input type="email" name="email" id="email" className="input w-[94%] sm:w-[80%]" />
+            <input
+              type="email"
+              name="email"
+              id="email"
+              className="input w-[94%] sm:w-[80%]"
+            />
             <label htmlFor="text" className="label">
               Message:
             </label>
-            <textarea name="message" id="message" className="input w-[94%] sm:w-[80%]" />
+            <textarea
+              name="message"
+              id="message"
+              className="input w-[94%] sm:w-[80%]"
+            />
             <button
               type="submit"
               className="btn hover:translate-y-3 ease-in-out duration-500 ml-3 sm:ml-2 mt-5 mb-1"
@@ -69,31 +85,31 @@ export default function Contact() {
 
         <div className="flex flex-row flex-wrap sm:flex-col mt-9 justify-center">
           <a
-            href="#"
+            href="https://twitter.com/morizuq"
             className="hover:translate-x-[-0.75rem] mr-6 ease-in-out duration-500"
           >
             <ImTwitter className="btn-ct h-[2.5rem] w-[2.5rem] p-[.5rem] lg:p-[1rem] lg:h-[4rem] lg:w-[4rem] mb-6" />
           </a>
           <a
-            href="#"
+            href="https://www.linkedin.com/in/morizuq/"
             className="hover:translate-x-[-0.75rem] mr-6 ease-in-out duration-500"
           >
             <GrLinkedin className="btn-ct h-[2.5rem] w-[2.5rem] p-[.5rem] lg:p-[1rem] lg:h-[4rem] lg:w-[4rem] mb-6" />
           </a>
           <a
-            href="#"
+            href="https://github.com/Morizuq"
             className="hover:translate-x-[-0.75rem] mr-6 ease-in-out duration-500"
           >
             <FaGithub className="btn-ct h-[2.5rem] w-[2.5rem] p-[.5rem] lg:p-[1rem] lg:h-[4rem] lg:w-[4rem] mb-6" />
           </a>
           <a
-            href="#"
+            href="https://wa.me/qr/C7HVE3ZJMKBRO1"
             className="hover:translate-x-[-0.75rem] mr-6 ease-in-out duration-500"
           >
             <BsWhatsapp className="btn-ct h-[2.5rem] w-[2.5rem] p-[.5rem] lg:p-[1rem] lg:h-[4rem] lg:w-[4rem] mb-6" />
           </a>
           <a
-            href="#"
+            href="mailto:morizuqshoneye@gmail.com"
             className="hover:translate-x-[-0.75rem] mr-6 ease-in-out duration-500"
           >
             <MdEmail className="btn-ct h-[2.5rem] w-[2.5rem] p-[.5rem] lg:p-[1rem] lg:h-[4rem] lg:w-[4rem] " />
