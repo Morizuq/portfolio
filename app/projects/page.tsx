e4865faf524fd7a341a6b7e00fde44a7d3ed155a
Morizuq <morizuqshoneye@gmail.com>
2023-05-10 12:43:41 +0100

    refactor

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -5,12 +5,6 @@ import { useEffect, useState } from "react";
 
 const cardObj = [
   {
-    name: "Natours",
-    link: "https://adventurous-pleat-foal.cyclic.app",
-    img: "/natback1.png",
-    gitlink: "https://github.com/Morizuq/Natours",
-  },
-  {
     name: "Natours-CSS",
     link: "https://css-nat.netlify.app",
     img: "/natours.png",
@@ -18,7 +12,7 @@ const cardObj = [
   },
   {
     name: "Nexter",
-    link: "https://css-nat.netlify.app",
+    link: "https://nexter-css.netlify.app",
     img: "/nexter1.png",
     gitlink: "https://github.com/Morizuq/Nexter-css",
   },
@@ -27,6 +21,12 @@ const cardObj = [
     link: "https://trillio-css.netlify.app",
     img: "/trillio.png",
     gitlink: "https://github.com/Morizuq/Trillo-css",
+  },
+  {
+    name: "Natours",
+    link: "https://adventurous-pleat-foal.cyclic.app",
+    img: "/natback1.png",
+    gitlink: "https://github.com/Morizuq/Natours",
   },
 ];
 
@@ -47,10 +47,12 @@ export default function Projects() {
     fetchRepos();
   }, []);
 
-  const list = repos.slice(0, 4)
+  const list = repos.slice(0, 4);
   return (
     <>
-    <h1 className="text-center sm:text-4xl text-3xl grad uppercase">Projects</h1>
+      <h1 className="text-center sm:text-4xl text-3xl grad uppercase">
+        Projects
+      </h1>
       <div className="p-8 flex flex-row flex-wrap justify-around">
         {cardObj.map((el) => (
           <Card {...el} />
